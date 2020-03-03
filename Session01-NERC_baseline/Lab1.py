f104d4b2538e2920e63d7b8a4abf7b512a63a3be
lluisg <lluis.guardia.f@gmail.com>
2020-03-03 11:47:23 +0100

    First complete run but not good

--- Session01-NERC_baseline/Lab1.py
+++ Session01-NERC_baseline/Lab1.py
@@ -4,7 +4,9 @@ import os
 
 
 def parseXML(file):
-    path = "/home/miguel/University/AHLT/Lab/labAHLT/data/Train/"
+    # path = "/home/miguel/University/AHLT/Lab/labAHLT/data/Train/"
+    path = "C:/Users/Lluis/Desktop/MATT/AHLT/LaboratoryAHLT/Lab1/data/Train/"
+
     dom = parse(path + file)
     return dom.getElementsByTagName("sentence")
 
@@ -16,6 +18,9 @@ def get_sentence_info(sentence):
 def tokenize(text):
     span_generator = WhitespaceTokenizer().span_tokenize(text)
     token_generator = WhitespaceTokenizer().tokenize(text)
+    for token, span in zip(token_generator, span_generator):
+        if token!=text[span[0]:span[1]]:
+            print('Falseee')
 
     tokens = [(token, span[0], span[1]) for token, span in zip(token_generator, span_generator)]
 
@@ -34,27 +39,35 @@ def extract_entities(token_list):
     return list_entities
 
 
-def evaluate(inputdir, outputdir):
+def evaluate(inputdir, outputfile):
     os.system("java -jar eval/evaluateNER.jar " + inputdir + " " + outputfile)
 
+def output_entities(id, entities, output):
+    for element in entities:
+        output.write(id+'|'+element["offset"]+'|'+element["name"]+'|'+element["type"]+'\n')
 
 def nerc(inputdir, outputfile):
-    for file in inputdir:
+    if os.path.exists(inputdir):
+        inputfiles = os.listdir(inputdir)
+    if os.path.exists(outputfile):
+        os.remove(outputfile)
+    f = open(outputfile, "a")
+    for file in inputfiles:
         tree = parseXML(file)
         for sentence in tree:
             (id, text) = get_sentence_info(sentence)
             token_list = tokenize(text)
             entities = extract_entities(token_list)
-            print(entities)
-            """
-            output_entities(id, entities, outputfile)
-    evaluate(inputdir, outpufile)
-    """
+            # print(entities)
+            output_entities(id, entities, f)
+    f.close()
+    evaluate(inputdir, outputfile)
 
 
 def main():
-    inputdir = os.listdir("/home/miguel/University/AHLT/Lab/labAHLT/data/Train/")
-    outputfile = "./output.txt"
+    # inputdir = os.listdir("/home/miguel/University/AHLT/Lab/labAHLT/data/Train/")
+    inputdir = "C:/Users/Lluis/Desktop/MATT/AHLT/LaboratoryAHLT/Lab1/data/Train/"
+    outputfile = "./task9.1_lluis_1.txt"
     nerc(inputdir, outputfile)
 
 
