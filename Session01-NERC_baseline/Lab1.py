c77fa9e72564b7765bc4379a6d9fd21726396190
michael-good <buenosnchez.miguelangelbs.migu@gmail.com>
2020-03-03 15:20:30 +0100

    Modified so that the program works

--- Session01-NERC_baseline/Lab1.py
+++ Session01-NERC_baseline/Lab1.py
@@ -3,11 +3,8 @@ from nltk.tokenize import WhitespaceTokenizer
 import os
 
 
-def parseXML(file):
-    # path = "/home/miguel/University/AHLT/Lab/labAHLT/data/Train/"
-    path = "C:/Users/Lluis/Desktop/MATT/AHLT/LaboratoryAHLT/Lab1/data/Train/"
-
-    dom = parse(path + file)
+def parseXML(file, inputdir):
+    dom = parse(inputdir + file)
     return dom.getElementsByTagName("sentence")
 
 
@@ -18,9 +15,6 @@ def get_sentence_info(sentence):
 def tokenize(text):
     span_generator = WhitespaceTokenizer().span_tokenize(text)
     token_generator = WhitespaceTokenizer().tokenize(text)
-    for token, span in zip(token_generator, span_generator):
-        if token!=text[span[0]:span[1]]:
-            print('Falseee')
 
     tokens = [(token, span[0], span[1]) for token, span in zip(token_generator, span_generator)]
 
@@ -42,18 +36,23 @@ def extract_entities(token_list):
 def evaluate(inputdir, outputfile):
     os.system("java -jar eval/evaluateNER.jar " + inputdir + " " + outputfile)
 
+
 def output_entities(id, entities, output):
     for element in entities:
         output.write(id+'|'+element["offset"]+'|'+element["name"]+'|'+element["type"]+'\n')
 
+
 def nerc(inputdir, outputfile):
+    '''
     if os.path.exists(inputdir):
         inputfiles = os.listdir(inputdir)
     if os.path.exists(outputfile):
         os.remove(outputfile)
+        '''
+    inputfiles = os.listdir(inputdir)
     f = open(outputfile, "a")
     for file in inputfiles:
-        tree = parseXML(file)
+        tree = parseXML(file, inputdir)
         for sentence in tree:
             (id, text) = get_sentence_info(sentence)
             token_list = tokenize(text)
@@ -65,8 +64,7 @@ def nerc(inputdir, outputfile):
 
 
 def main():
-    # inputdir = os.listdir("/home/miguel/University/AHLT/Lab/labAHLT/data/Train/")
-    inputdir = "C:/Users/Lluis/Desktop/MATT/AHLT/LaboratoryAHLT/Lab1/data/Train/"
+    inputdir = "./data/Train/"
     outputfile = "./task9.1_lluis_1.txt"
     nerc(inputdir, outputfile)
 
