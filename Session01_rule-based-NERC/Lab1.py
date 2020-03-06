001fae75a2311712749384f6783099234e08f67d
lluisg <lluis.guardia.f@gmail.com>
2020-03-06 01:36:19 +0100

    Limpiado tokens, algunos mas y 0.5

--- Session01_rule-based-NERC/Lab1.py
+++ Session01_rule-based-NERC/Lab1.py
@@ -21,43 +21,99 @@ def tokenize(text):
     return tokens
 
 
-def return_type(previous, text):
+def return_type(text, prev):
     type = "other"
 
     if (text.isupper() or "aspirin" in text.lower() or "PEGA" in text or text.startswith("SP") or "XX" in text or
         "IVA" in text):
         type = "brand"
 
-    elif text.endswith('phane') or text[0].isdigit() or "MC" in text or "gain" in text:
+    elif (text.endswith('phane') or "MC" in text or "gaine" in text or
+           (len(text)>16 and ((text[0].isdigit() and text[1]=='-') or (text[0].isdigit() and text[1].isdigit() and text[2]=='-'))) or #1- or 16-
+           (len(text)>10 and ((text[0].isdigit() and text[1]==',' and text[2].isdigit()))) ): #1,3
         type = "drug_n"
 
-    elif "thiazide" in text or text.startswith("anti") or "cont" in text or "ure" in text or "ids" in text:
+    elif ("thiazide" in text or text.startswith("anti") or "cont" in text or "ure" in text or "ids" in text or
+         text.startswith('beta-block') or text.startswith('Beta-block') or text.startswith('Beta-Block') ):
         type = "group"
 
     elif (text.endswith('azole') or text.endswith('ine') or text.endswith('amine') or
           text.endswith('mycin') or text.endswith('avir') or text.endswith('ide') or text.endswith('olam') or
+          text.startswith('z') or
           "hydr" in text or "in" in text or text.startswith("amph") or "cyclo" in text or "ole" in text or
           "ano" in text or "ium" in text):
         type = "drug"
+
+    # if it has the prefix word, it returns a special case
+    elif text.startswith('beta') or text.startswith('Beta') :
+        type = 'previous'
+
+    elif text.startswith('agent') or text.startswith('Agent'):
+        type = 'future'
 
     if len(text) <= 4:
         type = "other"
 
     return type
-
+def check_edges_letter(element):
+    type(element)
+    if len(element)>1:
+        if not str(element[-1]).isdigit():
+          if not str(element[-1]).isalpha(): #check if last item is a letter or number
+            if not str(element[-1])=='%':
+                return 'end'
+        elif not str(element[0]).isdigit():
+          if not str(element[0]).isalpha(): #check if last item is a letter or number
+            if not str(element[-1])=='-':
+                return 'start'
+    return 'none'
 
 def extract_entities(token_list):
-    previous = ""
+    previous = False
     list_entities = []
+    #demands as implementation first
+    auxiliar = {"name": '',
+              "offset": str(0),
+              "type": ''
+              }
     for index, element in enumerate(token_list):
-        type = return_type(previous, element[0])
-        if type != "other":
+
+        modified=False
+        # check if last letter is a number or alphabet
+        while check_edges_letter(element[0]) == 'end':
+            y = list(element) #convert to a list
+            y[0] = y[0][0:-1] #change value since tuple are immutable
+            element = tuple(y) #convert back to tuple
+        # check if last letter is a number or alphabet
+        while check_edges_letter(element[0]) == 'start':
+            y = list(element)
+            y[0] = y[0][1:]
+            element = tuple(y)
+
+        type = return_type(element[0], previous)
+
+        if previous == True or type=='future': #the revious word was type='previous'
+            entity = {"name": auxiliar['name']+' '+element[0],
+                      "offset": auxiliar['offset'] + "-" + str(element[2]),
+                      "type": 'group'
+                      }
+            list_entities.append(entity)
+            previous = False
+
+        elif type == 'previous': #if it's the previous word, we will only set the flag
+            previous = True
+
+        elif type != "other":
             entity = {"name": element[0],
                       "offset": str(element[1]) + "-" + str(element[2]),
                       "type": type
                       }
             list_entities.append(entity)
-        previous = type
+        # auxiliar element to get previous and future words
+        auxiliar = {"name": element[0],
+                  "offset": str(element[1]),
+                  "type": type
+                  }
     return list_entities
 
 
@@ -89,7 +145,7 @@ def nerc(inputdir, outputfile):
 
 def main():
     inputdir = "./data/Train/"
-    outputfile = "./task9.1_lluis_1.txt"
+    outputfile = "./task9.1_lluis_3.txt"
     nerc(inputdir, outputfile)
 
 
