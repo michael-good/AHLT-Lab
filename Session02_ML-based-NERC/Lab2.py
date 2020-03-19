9cf9b529ccc09a39e1ed5e504b07ea9766154111
lluisg <lluis.guardia.f@gmail.com>
2020-03-19 02:40:43 +0100

    Lab2 labels basic

--- Session02_ML-based-NERC/Lab2.py
+++ Session02_ML-based-NERC/Lab2.py
@@ -1,20 +1,21 @@
-from xml.dom.minidom import parse
 import os, sys
-from nltk.tokenize import TreebankWordTokenizer as twt
 import pycrfsuite
+from nltk.tokenize import TreebankWordTokenizer as twt
+from IPython import embed
+from xml.etree import ElementTree
 
 def parseXML(file, inputdir):
-    dom = parse(inputdir + file)
-    return dom.getElementsByTagName("sentence")
+    tree = ElementTree.parse(inputdir+file)
+    return tree.getroot()
 
 def get_sentence_info(sentence):
-    return sentence.getAttribute("id"), sentence.getAttribute("text")
+    return sentence.attrib["id"], sentence.attrib["text"]
 
 def tokenize(text):
     span_generator = twt().span_tokenize(text)
     tokens = []
     for s in span_generator:
-        tokens.append((text[s[0]:s[1]], s[0], s[1]))
+        tokens.append((text[s[0]:s[1]], s[0], s[1]-1))
     return tokens
 
 def extract_features(s):
@@ -67,11 +68,35 @@ def extract_features(s):
 
     return features
 
-def output_features(id, s, ents):
-    #type of element, for now all B-drug
-    elem = 'B-drug'
+def get_ground_truth_label(token_list, sentence):
+    list_ofset = []
+    entities = sentence.getchildren()
+    if len(entities)!=0:
+        for entity in entities:
+            if entity.tag=='entity':
+                list_ofset.append(entity.attrib['charOffset'].split('-'))
+
+    ground = []
+    for token in token_list:
+        g='O'
+        for ind, off in enumerate(list_ofset):
+            if len(off[1].split(';'))==1:
+                if token[1]>=int(off[0]) and token[2]<=int(off[1]):
+                    g=entities[ind].attrib['type']
+
+            else:
+                sep_off = [off[0], off[1].split(';')[0], off[1].split(';')[1], off[2]]
+                if token[1]>=int(sep_off[0]) and token[2]<=int(sep_off[1]):
+                    g=entities[ind].attrib['type']
+                if token[1]>=int(sep_off[2]) and token[2]<=int(sep_off[3]):
+                    g=entities[ind].attrib['type']
+
+        ground.append(g)
+    return ground
+
+def output_features(id, s, ents, gold_class):
     for ind, token in enumerate(s):
-        sys.stdout.write( id +'\t'+ token[0] +'\t'+ str(token[1]) +'\t'+ str(token[2]) +'\t'+ elem)
+        sys.stdout.write( id +'\t'+ token[0] +'\t'+ str(token[1]) +'\t'+ str(token[2]) +'\t'+ gold_class[ind])
         for feat in ents[ind]:
             sys.stdout.write('\t'+ str(feat))
         sys.stdout.write('\n')
@@ -90,15 +115,8 @@ def train(traindata, labels):
         'feature.possible_transitions': True
     })
 
-    trainer = pycrfsuite.CRF(
-        algorithm='lbfgs',
-        c1=0.1, # coefficient for L1 penalty
-        c2=0.1, # coefficient for L2 penalty
-        max_iterations=100, # stop earlier
-        all_possible_transitions=True) # include transitions that are possible, but not observed
-
-    trainer.train('lab2_model_trained.crfsuite')
-    trainer.logparser.last_iteration
+    trainer.train('model_trainedL2.crfsuite')
+    print(trainer.logparser.last_iteration)
 
 
 def main():
@@ -107,16 +125,19 @@ def main():
     if os.path.exists(traindir):
         trainfiles = os.listdir(traindir)
     traindata = [] #where all the features are saved
+    labels = [] #where the ground truth of the train data labels will be
     for file in trainfiles:
         # print(traindir+file)
-        tree = parseXML(file, traindir)
-        for sentence in tree:
+        root = parseXML(file, traindir)
+
+        for sentence in root:
             (id, text) = get_sentence_info(sentence)
             token_list = tokenize(text)
             features = extract_features(token_list)
+            type_ground = get_ground_truth_label(token_list, sentence)
             traindata.append(features)
-            output_features(id, token_list, features)
-    # NOSE DON SURTEN ELS LABELS
+            labels.append(type_ground)
+            # output_features(id, token_list, features, type_ground)
     train(traindata, labels)
 
     # #PREDICT
