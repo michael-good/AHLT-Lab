d0323a60d3517a09c3075c34e700319c4eb6cda3
lluisg <lluis.guardia.f@gmail.com>
2020-03-19 19:42:41 +0100

    Some modifications L2

--- Session02_ML-based-NERC/Lab2.py
+++ Session02_ML-based-NERC/Lab2.py
@@ -8,8 +8,10 @@ def parseXML(file, inputdir):
     tree = ElementTree.parse(inputdir+file)
     return tree.getroot()
 
+
 def get_sentence_info(sentence):
     return sentence.attrib["id"], sentence.attrib["text"]
+
 
 def tokenize(text):
     span_generator = twt().span_tokenize(text)
@@ -17,6 +19,7 @@ def tokenize(text):
     for s in span_generator:
         tokens.append((text[s[0]:s[1]], s[0], s[1]-1))
     return tokens
+
 
 def extract_features(s):
     features = []
@@ -65,8 +68,8 @@ def extract_features(s):
                 feat.append('capitalized')
 
         features.append(feat)
-
     return features
+
 
 def get_ground_truth_label(token_list, sentence):
     list_ofset = []
@@ -77,22 +80,41 @@ def get_ground_truth_label(token_list, sentence):
                 list_ofset.append(entity.attrib['charOffset'].split('-'))
 
     ground = []
-    for token in token_list:
+    previous=0
+    for ind2, token in enumerate(token_list):
         g='O'
         for ind, off in enumerate(list_ofset):
             if len(off[1].split(';'))==1:
                 if token[1]>=int(off[0]) and token[2]<=int(off[1]):
                     g=entities[ind].attrib['type']
+                    previous+=1
+                else:
+                    previous=0
 
             else:
                 sep_off = [off[0], off[1].split(';')[0], off[1].split(';')[1], off[2]]
                 if token[1]>=int(sep_off[0]) and token[2]<=int(sep_off[1]):
                     g=entities[ind].attrib['type']
-                if token[1]>=int(sep_off[2]) and token[2]<=int(sep_off[3]):
+                    previous+=1
+                elif token[1]>=int(sep_off[2]) and token[2]<=int(sep_off[3]):
                     g=entities[ind].attrib['type']
+                    previous+=1
+                else:
+                    previous=0
 
+
+        if g != 'O':
+            if previous == 1:
+                g='B-'+g
+            else:
+                g='I-'+g
         ground.append(g)
+
+        # if previous>1:
+        #     print(previous, token_list[ind2-(previous-1)], ground[ind2-(previous-1)], token, g)
+
     return ground
+
 
 def output_features(id, s, ents, gold_class):
     for ind, token in enumerate(s):
@@ -100,6 +122,7 @@ def output_features(id, s, ents, gold_class):
         for feat in ents[ind]:
             sys.stdout.write('\t'+ str(feat))
         sys.stdout.write('\n')
+
 
 def train(traindata, labels):
     trainer = pycrfsuite.Trainer(verbose=False)
@@ -118,6 +141,52 @@ def train(traindata, labels):
     trainer.train('model_trainedL2.crfsuite')
     print(trainer.logparser.last_iteration)
 
+def predict(feat):
+    predictor = pycrfsuite.Tagger()
+    predictor.open('model_trainedL2.crfsuite')
+    return predictor.tag(feat)
+
+
+def output_entities(id_, token_list, pred, foutput):
+    wait = False #while it's waiting will not print the elements
+    name = ''
+    off_start = '0'
+    for ind, token in enumerate(token_list):
+        print(token, pred[ind])
+        if pred[ind]=='O':
+            wait = True
+        elif( (pred[ind].startswith('B') and pred[ind+1].startswith('O')) or
+                (pred[ind].startswith('B') and pred[ind+1].startswith('B')) ):
+            element = {'name': token[0],
+                      'offset': str(token[1]) + '-' + str(token[2]),
+                      'type': pred[ind].split('-')[1] #without B or I
+                      }
+            wait = False
+        elif pred[ind].startswith('B') and pred[ind+1].startswith('I'):
+            name = token[0]
+            off_start = str(token[1])
+            wait = True
+        elif( (pred[ind].startswith('I') and pred[ind+1].startswith('O')) or
+            (pred[ind].startswith('I') and pred[ind+1].startswith('B')) ):
+            element = {'name': name+' '+token[0],
+                      'offset': off_start + '-' + str(token[2]),
+                      'type': pred[ind].split('-')[1]
+                      }
+            wait = False
+        elif pred[ind].startswith('I') and pred[ind+1].startswith('I'):
+            name = name+' '+token[0]
+            wait = True
+        else:
+            print('There have been some erroooooooooooooooooooooor')
+
+        if not wait:
+            # print(element)
+            foutput.write(id_ + '|' + element['offset'] + '|' + element['name'] + '|' + element['type'] + '\n')
+
+
+def evaluate(input_dir, output_file):
+    os.system('java -jar eval/evaluateNER.jar ' + input_dir + ' ' + output_file)
+
 
 def main():
     # TRAIN
@@ -129,7 +198,6 @@ def main():
     for file in trainfiles:
         # print(traindir+file)
         root = parseXML(file, traindir)
-
         for sentence in root:
             (id, text) = get_sentence_info(sentence)
             token_list = tokenize(text)
@@ -141,21 +209,24 @@ def main():
     train(traindata, labels)
 
     # #PREDICT
+    testdir = "./data/Train/"
     # testdir = "./data/Devel/"
-    # outputfile = "./task9.2_lluis_1.txt"
-    # if os.path.exists(testdir):
-    #     testfiles = os.listdir(testdir)
-    # f = open(outputfile, "a")
-    # for file in testfiles:
-    #     print(testdir+file)
-    #     tree = parseXML(file, testdir)
-    #     for sentence in tree:
-    #         (id, text) = get_sentence_info(sentence)
-    #         token_list = tokenize(text)
-    #         features = extract_features(token_list)
-    #         y_pred = crf.predict(features)
-    #         output_entities(id, token_list, y_pred, f)
-    # f.close()
+    outputfile = "./task9.2_lluis_1.txt"
+    if os.path.exists(testdir):
+        testfiles = os.listdir(testdir)
+    testdata = [] #where all the features are saved
+    labels = [] #where the ground truth of the train data labels will be
+    f = open(outputfile, "a")
+    for file in testfiles:
+        print(testdir+file)
+        root = parseXML(file, testdir)
+        for sentence in root:
+            (id, text) = get_sentence_info(sentence)
+            token_list = tokenize(text)
+            features = extract_features(token_list)
+            y_pred = predict(features)
+            output_entities(id, token_list, y_pred, f)
+    f.close()
     # evaluate(testdir, outputfile)
 
 if __name__ == "__main__":
