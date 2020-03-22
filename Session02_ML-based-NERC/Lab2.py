a2d3d21a8e960abfd242d914cc398b2a4adba020
lluisg <lluis.guardia.f@gmail.com>
2020-03-22 13:28:05 +0100

    Predict functions strange

--- Session02_ML-based-NERC/Lab2.py
+++ Session02_ML-based-NERC/Lab2.py
@@ -1,8 +1,12 @@
-import os, sys
+import os
 import pycrfsuite
 from nltk.tokenize import TreebankWordTokenizer as twt
 from xml.etree import ElementTree
 from extract_featuresL2 import extract_features
+
+# True will obtain features from the external files too (goal5)
+# If false will not obtain them (goal4)
+EXTERNAL_FILE = False
 
 def parseXML(file, inputdir):
     tree = ElementTree.parse(inputdir+file)
@@ -31,7 +35,7 @@ def get_ground_truth_label(token_list, sentence):
     ground = []
     previous=0
     for ind2, token in enumerate(token_list):
-        g='O'
+        g='O' #assign O first, if it's any kind of drug it will stay like this
         for ind, off in enumerate(list_ofset):
             if len(off[1].split(';'))==1:
                 if token[1]>=int(off[0]) and token[2]<=int(off[1]):
@@ -39,7 +43,8 @@ def get_ground_truth_label(token_list, sentence):
                     previous+=1
                 else:
                     previous=0
-            else:
+            else:   # there are some elements assigned to more than one
+                    # to these elements only assign the last tag that have appeared
                 sep_off = [off[0], off[1].split(';')[0], off[1].split(';')[1], off[2]]
                 if token[1]>=int(sep_off[0]) and token[2]<=int(sep_off[1]):
                     g=entities[ind].attrib['type']
@@ -50,6 +55,7 @@ def get_ground_truth_label(token_list, sentence):
                 else:
                     previous=0
 
+        # add the BI tags
         if g != 'O':
             if previous == 1:
                 g='B-'+g
@@ -60,17 +66,17 @@ def get_ground_truth_label(token_list, sentence):
     return ground
 
 
-def output_features(id, s, ents, gold_class):
+def output_features(id, s, ents, gold_class, trainfile):
     for ind, token in enumerate(s):
-        sys.stdout.write( id +'\t'+ token[0] +'\t'+ str(token[1]) +' '+ str(token[2]) +'\t'+ gold_class[ind] +'\t')
+        sentence_features = id +'\t'+ token[0] +'\t'+ str(token[1]) +' '+ str(token[2]) +'\t'+ gold_class[ind] +'\t'
         for feat in ents[ind]:
-            sys.stdout.write(' '+ str(feat))
-        sys.stdout.write('\n')
+            sentence_features = sentence_features+' '+ str(feat)
+        sentence_features = sentence_features+'\n'
+        trainfile.write(sentence_features)
 
 
 def train(traindata, labels):
     trainer = pycrfsuite.Trainer(verbose=False)
-    i=0
     for xseq, yseq in zip(traindata, labels):
         trainer.append(xseq, yseq)
 
@@ -79,17 +85,18 @@ def train(traindata, labels):
         'c2': 1e-3,  # coefficient for L2 penalty
         # as we have a considerable quantity of features to train from
         # the training should be longer
-        'max_iterations': 2500,
+        'max_iterations': 3000,
         # include transitions that are possible, but not observed
         'feature.possible_transitions': True
     })
 
-    trainer.train('model_trainedL2.crfsuite')
+    trainer.train('model_trained_goal4.crfsuite')
     print(trainer.logparser.last_iteration)
 
 def predict(feat):
+    # predicts the tag for the word given the features
     predictor = pycrfsuite.Tagger()
-    predictor.open('model_trainedL2.crfsuite')
+    predictor.open('model_trained_goal4.crfsuite')
     return predictor.tag(feat)
 
 
@@ -151,48 +158,57 @@ def output_entities(id_, token_list, pred, foutput):
 def evaluate(input_dir, output_file):
     os.system('java -jar eval/evaluateNER.jar ' + input_dir + ' ' + output_file)
 
-def external_prediction(feat):
-    return False
-
 
 def main():
     # TRAIN
     traindir = "./data/Train/"
+    if EXTERNAL_FILE:
+        trainfile = "./train_elements_9.5_lluis_1.txt"
+    else:
+        trainfile = "./train_elements_9.5_lluis_1.txt"
     if os.path.exists(traindir):
         trainfiles = os.listdir(traindir)
     traindata = [] #where all the features are saved
     labels = [] #where the ground truth of the train data labels will be
+    t = open(trainfile, "a")
     for file in trainfiles:
         root = parseXML(file, traindir)
         for sentence in root:
             (id, text) = get_sentence_info(sentence)
             token_list = tokenize(text)
-            features = extract_features(token_list)
-            type_ground = get_ground_truth_label(token_list, sentence)
+            features = extract_features(token_list, EXTERNAL_FILE)
             traindata.append(features)
+            type_ground = get_ground_truth_label(token_list, sentence)
             labels.append(type_ground)
-            output_features(id, token_list, features, type_ground)
+            output_features(id, token_list, features, type_ground, t)
     train(traindata, labels)
+    t.close()
 
-    # PREDICT
-    for testdir in ["./data/Train/", "./data/Devel/"]:
-        outputfile = "./task9.2_lluis_1.txt"
-        if os.path.exists(testdir):
-            testfiles = os.listdir(testdir)
-        testdata = [] #where all the features are saved
-        f = open(outputfile, "a")
-        for file in testfiles:
-            root = parseXML(file, testdir)
-            for sentence in root:
-                (id, text) = get_sentence_info(sentence)
-                token_list = tokenize(text)
-                features = extract_features(token_list)
-                external = external_prediction(features)
-                if external==False:
+    for n in range(6):
+        # PREDICT
+        #two different files for the two goals of the lab2
+        if EXTERNAL_FILE:
+            output = "./task9.5_lluis_1"
+        else:
+            output = "./task9.4_lluis_1"
+        # for test in ["Devel", "Test-NER", "Test-DDI"]:
+        for test in ["Devel"]:
+            outputfile = output+'_'+test+'.txt'
+            f = open(outputfile, "a")
+            testdir = "./data/"+test+"/"
+            if os.path.exists(testdir):
+                testfiles = os.listdir(testdir)
+            testdata = [] #where all the features are saved
+            for file in testfiles:
+                root = parseXML(file, testdir)
+                for sentence in root:
+                    (id, text) = get_sentence_info(sentence)
+                    token_list = tokenize(text)
+                    features = extract_features(token_list, EXTERNAL_FILE)
                     y_pred = predict(features)
-                output_entities(id, token_list, y_pred, f)
-        f.close()
-        evaluate(testdir, outputfile)
+                    output_entities(id, token_list, y_pred, f)
+            f.close()
+            evaluate(testdir, outputfile)
 
 if __name__ == "__main__":
     main()
