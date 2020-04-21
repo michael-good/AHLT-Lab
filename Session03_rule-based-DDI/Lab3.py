324741c5c89783eabd08097b6b6c40d2cd25ca6b
michael-good <buenosnchez.miguelangelbs.migu@gmail.com>
2020-04-21 11:34:00 +0200

    Refactored some code, added comments and docstrings

--- Session03_rule-based-DDI/Lab3.py
+++ Session03_rule-based-DDI/Lab3.py
@@ -1,74 +1,113 @@
-from xml.dom.minidom import parse
 import os
+import time
+from xml.dom.minidom import parse
 from interactions import check_interaction
 # import nltk CoreNLP module (just once)
 from nltk.parse.corenlp import CoreNLPDependencyParser
 # connect to your CoreNLP server (just once)
 my_parser = CoreNLPDependencyParser(url="http://localhost:9000")
 
+
 def parse_xml(file):
+    """
+        Parses given XML file
+
+        Parameters:
+            file: name of the file to parse
+        Returns:
+            Data structure holding 'file' data in a readable way
+    """
     return parse(file)
 
-def getOffsets(sentence, word):
+
+def get_offsets(sentence, word):
+    """
+        Retrieves the position of word within sentence at character level
+
+        Parameters:
+            sentence: information held by 'sentence' tag structure in XML file
+            word: string of a word that belongs to the provided sentence
+        Returns:
+            Start and end index of the position of word within sentence at character level
+    """
     start_index = sentence.find(word)
-    end_index = start_index + len(word) -1 # if the start_index is not -1
+    # if the start_index is not -1
+    end_index = start_index + len(word) - 1
     return start_index, end_index
 
+
 def analyze(sentence):
+    """
+        Generates a grammar dependency tree of all the words within the sentence
+
+        Parameters:
+            sentence: information held by 'sentence' tag structure in XML file
+        Returns:
+            A grammar dependency tree
+    """
     # parse text (as many times as needed)
-    mytree, = my_parser.raw_parse(sentence)
+    tree, _ = my_parser.raw_parse(sentence)
 
     # enrich the NLPDepencyGraph with the start and end offset
-    for e in range(1, len(mytree.nodes)):
-        node = mytree.nodes[e]
+    for e in range(1, len(tree.nodes)):
+        node = tree.nodes[e]
         word = node['word']
-        start_off, end_off = getOffsets(sentence, word)
+        start_off, end_off = get_offsets(sentence, word)
         # returns start_off=-1 if didn't find the word in the sentence
         if start_off != -1:
             node['start'] = start_off
             node['end'] = end_off
 
-    return mytree
+    return tree
 
 
-def evaluate(inputdir,outputfile):
-    os.system("java -jar eval/evaluateDDI.jar " + inputdir + " " + outputfile)
+def evaluate(input_dir, output_file):
+    """
+        Evaluates the performance of the classifier by means of different metrics such as
+        precision, recall and f-score
 
-import time
+        Parameters:
+            input_dir: path to output_file
+            output_file: txt file holding entities data
+    """
+    os.system("java -jar eval/evaluateDDI.jar " + input_dir + " " + output_file)
+
+
 def main():
     start = time.time()
 
-    # inputdir = './data/Devel'
-    # outputfile = 'task9.2_lluis_Devel.txt'
+    # input_dir = './data/Devel'
+    # output_file = 'task9.2_lluis_Devel.txt'
 
-    # inputdir = './data/Test-NER'
-    # outputfile = 'task9.2_lluis_TestNER.txt'
+    # input_dir = './data/Test-NER'
+    # output_file = 'task9.2_lluis_TestNER.txt'
 
-    inputdir = './data/Test-DDI'
-    outputfile = 'task9.2_lluis_TestDDI.txt'
+    input_dir = './data/Test-DDI'
+    output_file = 'task9.2_lluis_TestDDI.txt'
 
-    foutput = open(outputfile, "w")
+    foutput = open(output_file, "w")
+
     # process each file in directory
-    for f in os.listdir(inputdir) :
+    for f in os.listdir(input_dir) :
         # print(f)
         # parse XML file, obtaining a DOM tree
-        tree = parse(inputdir + "/" + f)
+        tree = parse(input_dir + "/" + f)
         # process each sentence in the file
         sentences = tree.getElementsByTagName("sentence")
-        for s in sentences :
-            sid = s.attributes["id"].value # get sentence id
-            stext = s.attributes["text"].value # get sentence text
+        for s in sentences:
+            # get sentence id
+            sid = s.attributes["id"].value
+            # get sentence text
+            stext = s.attributes["text"].value
             # it gives an error on the raw_parser if the sentence is empty ("")
             if stext != "":
                 # load sentence entities into a dictionary
                 entities = {}
-                entities_name = {}
-                entities_type = {}
-                ents = s.getElementsByTagName("entity")
-                for e in ents :
-                    id = e.attributes["id"].value
-                    offs = e.attributes["charOffset"].value.split("-")
-                    entities[id] = offs
+                sentence_entities = s.getElementsByTagName("entity")
+                for e in sentence_entities:
+                    id_ = e.attributes["id"].value
+                    offset = e.attributes["charOffset"].value.split("-")
+                    entities[id_] = offset
 
                 # Tokenize, tag, and parse sentence
                 analysis = analyze(stext)
@@ -86,14 +125,12 @@ def main():
                     # else:
                     #     type_g = "null"
 
-                    (is_ddi,ddi_type) = check_interaction(analysis, entities, id_e1, id_e2)
-
+                    (is_ddi, ddi_type) = check_interaction(analysis, entities, id_e1, id_e2)
                     foutput.write("|".join([sid, id_e1, id_e2, str(is_ddi), ddi_type]))
                     foutput.write("\n")
-
     foutput.close()
     # get performance score
-    evaluate(inputdir,outputfile)
+    evaluate(input_dir, output_file)
 
 
 if __name__ == '__main__':
--- Session03_rule-based-DDI/interactions.py
+++ Session03_rule-based-DDI/interactions.py
@@ -1,14 +1,25 @@
 def check_interaction(analysis, entities, id_e1, id_e2):
+    """
+        Given two entities that belong to a sentence, check_interaction tries to find the type
+        of interaction between them if any
 
-    found_e1=False
-    found_e2=False
+        Parameters:
+            analysis: Grammar dependency tree
+            entities: List of all entities of the current sentence
+            id_e1: Entity of the sentence number 1 to compare
+            id_e2: Entity of the sentence number 2 to compare
+        Returns:
+            Whether there is interaction or not between two entities and its type
+    """
+    found_e1 = False
+    found_e2 = False
 
     # check they aren't multielements (that one word is used for more than one)
-    if len(entities[id_e1])==2 and len(entities[id_e2])==2:
-        starte1 = int(entities[id_e1][0])
-        ende1 = int(entities[id_e1][1])
-        starte2 = int(entities[id_e2][0])
-        ende2 = int(entities[id_e2][1])
+    if len(entities[id_e1]) == 2 and len(entities[id_e2]) == 2:
+        start_e1 = int(entities[id_e1][0])
+        end_e1 = int(entities[id_e1][1])
+        start_e2 = int(entities[id_e2][0])
+        end_e2 = int(entities[id_e2][1])
 
         # get info of the two entities
         for e in range(1, len(analysis.nodes)):
@@ -17,17 +28,15 @@ def check_interaction(analysis, entities, id_e1, id_e2):
                 start = node["start"]
                 end = node["end"]
 
-                if start >= starte1 and end <= ende1:
+                if start >= start_e1 and end <= end_e1:
                     found_e1 = True
                     node_entity1 = node
-                    numbere1=e
-                elif start >= starte2 and end <= ende2:
+                elif start >= start_e2 and end <= end_e2:
                     found_e2 = True
                     node_entity2 = node
-                    numbere2=e
 
         # (2) check if same parent
-        # check if 1under2 or 2under1 (1) has been analyzed but doesn't add nothing
+        # check if 1under2 or 2under1 (1) has been analyzed but doesn't add anything
         if found_e1 and found_e2:
             if node_entity1["head"] == node_entity2["head"]:
                 if node_entity1["rel"] == 'conj' and node_entity2["rel"] == 'conj':
@@ -39,39 +48,38 @@ def check_interaction(analysis, entities, id_e1, id_e2):
             if 'start' in node:
                 start = node["start"]
                 end = node["end"]
-                if start>=starte1 and end<=ende2:
-
+                if start >= start_e1 and end <= end_e2:
                     if 'V' in node["tag"]:
                         if (
-                         'VB' == node["tag"] and node["lemma"]=='increase' or
-                         'V' in node["tag"] and node["lemma"]=='decrease' or
-                         'V' in node["tag"] and node["lemma"]=='interfere' or
-                         'V' in node["tag"] and node["lemma"]=='inhibit' or
-                         'V' in node["tag"] and node["lemma"]=='alter' or
-                         'VB' == node["tag"] and node["lemma"]=='cause' or
-                         'V' in node["tag"] and node["lemma"]=='delay' or
-                         'V' in node["tag"] and node["lemma"]=='raise' ):
+                         'VB' == node["tag"] and node["lemma"] == 'increase' or
+                         'V' in node["tag"] and node["lemma"] == 'decrease' or
+                         'V' in node["tag"] and node["lemma"] == 'interfere' or
+                         'V' in node["tag"] and node["lemma"] == 'inhibit' or
+                         'V' in node["tag"] and node["lemma"] == 'alter' or
+                         'VB' == node["tag"] and node["lemma"] == 'cause' or
+                         'V' in node["tag"] and node["lemma"] == 'delay' or
+                         'V' in node["tag"] and node["lemma"] == 'raise'):
                             return 1, 'mechanism'
 
                         elif (
-                         'VBP' == node["tag"] and  node["lemma"] == 'include' or
-                         'V' in node["tag"] and  node["lemma"] == 'potentiate' or
-                         'VB' == node["tag"] and  node["lemma"] == 'enhance' or
-                         'VB' == node["tag"] and  node["lemma"] == 'reduce' or
-                         'VBZ' in node["tag"] and  node["lemma"] == 'produce' or
-                         'V' in node["tag"] and  node["lemma"] == 'antagonize' ):
+                         'VBP' == node["tag"] and node["lemma"] == 'include' or
+                         'V' in node["tag"] and node["lemma"] == 'potentiate' or
+                         'VB' == node["tag"] and node["lemma"] == 'enhance' or
+                         'VB' == node["tag"] and node["lemma"] == 'reduce' or
+                         'VBZ' in node["tag"] and node["lemma"] == 'produce' or
+                         'V' in node["tag"] and node["lemma"] == 'antagonize'):
                             return 1, 'effect'
 
                         elif (
-                         'V' in node["tag"] and node["lemma"]=='tell' or
-                         'VBN' == node["tag"] and node["lemma"]=='administer' or
-                         'V' in node["tag"] and node["lemma"]=='take' or
-                         'V' in node["tag"] and node["lemma"]=='exceed' ):
+                         'V' in node["tag"] and node["lemma"] == 'tell' or
+                         'VBN' == node["tag"] and node["lemma"] == 'administer' or
+                         'V' in node["tag"] and node["lemma"] == 'take' or
+                         'V' in node["tag"] and node["lemma"] == 'exceed'):
                             return 1, 'advise'
 
                         elif (
                          'VBZ' == node["tag"] and node["lemma"] == 'suggest' or
-                         'V' in node["tag"] and node["lemma"] == 'interact' ):
+                         'V' in node["tag"] and node["lemma"] == 'interact'):
                             return 1, 'int'
 
                         else:
